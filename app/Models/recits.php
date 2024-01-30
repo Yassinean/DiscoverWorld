c59885b96e71b9cbb678be41d1cf2b6679cd6745
Yassine Hanach <yassinedubraska@gmail.com>
2024-01-30 10:48:22 +0100

    Configuration WorkSpace

---
+++ app/Models/recits.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class recits extends Model
+{
+    use HasFactory;
+}
